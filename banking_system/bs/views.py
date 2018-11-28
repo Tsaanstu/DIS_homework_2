50d7fc1f52d4838082c0fead9f273fe5e89c83fe
Tsaanstu <atasunts.vladimir@yandex.ru>
2018-11-29 00:19:44 +0300

    faker client

--- banking_system/bs/views.py
+++ banking_system/bs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from bs.models import User, Client, Account, History_of_changes, Transfer, Rate
 
 def paginator(user_list, request):
     paginator = Paginator(user_list, 10)
@@ -15,28 +16,7 @@ def paginator(user_list, request):
 
 
 def index(request):
-    user_list = [
-        {"name": "Ivanov Ivan Number1", "id": 1},
-        {"name": "Ivanov Ivan Number2", "id": 2},
-        {"name": "Ivanov Ivan Number3", "id": 3},
-        {"name": "Ivanov Ivan Number4", "id": 4},
-        {"name": "Ivanov Ivan Number5", "id": 5},
-        {"name": "Ivanov Ivan Number6", "id": 6},
-        {"name": "Ivanov Ivan Number7", "id": 7},
-        {"name": "Ivanov Ivan Number8", "id": 8},
-        {"name": "Ivanov Ivan Number9", "id": 9},
-        {"name": "Ivanov Ivan Number10", "id": 10},
-        {"name": "Ivanov Ivan Number11", "id": 11},
-        {"name": "Ivanov Ivan Number12", "id": 12},
-        {"name": "Ivanov Ivan Number13", "id": 13},
-        {"name": "Ivanov Ivan Number14", "id": 14},
-        {"name": "Ivanov Ivan Number15", "id": 15},
-        {"name": "Ivanov Ivan Number16", "id": 16},
-        {"name": "Ivanov Ivan Number17", "id": 17},
-        {"name": "Ivanov Ivan Number18", "id": 18},
-        {"name": "Ivanov Ivan Number19", "id": 19},
-        {"name": "Ivanov Ivan Number20", "id": 20},
-    ]
+    user_list = Client.objects.all().order_by('full_name')
     user_list = paginator(user_list, request)
     return render(request, 'bs/index.html', {"users": user_list})
 
--- banking_system/do_faker.py
+++ banking_system/do_faker.py
@@ -1,39 +1,26 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, date, time
 from faker import Factory
-import codecs
-from django.core.management.base import BaseCommand
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
-from bs.models import Client
-from random import choice, randint
-import urllib.request as request
-from django.core.files import File
+import sqlite3
 
+def FAKE_CLIENT():
+	MAX = 115
+	conn = sqlite3.connect("db.sqlite3")
+	cursor = conn.cursor()
+	for i in range(0, MAX):
+		faker = Factory.create()
+		profile = faker.profile()
+		full_name = profile['name']
+		login = profile['username']
+		birthday = profile['birthdate']
+		contract_number = hash(login + str(birthday)) 
+		if contract_number < 0:
+			contract_number *= -1
+		date_conclusion = date.today()
+		tel = '+79999999999'
+		address = 'Moscow'
+		input_data = [(full_name, login, birthday, contract_number, date_conclusion, tel, address)]
+		cursor.executemany('INSERT INTO bs_client (full_name, login, birthday, contract_number, date_conclusion, tel, address) VALUES (?, ?, ?, ?, ?, ?, ?)', input_data)
+	conn.commit()
 
-class Command(BaseCommand):
-    help = 'Fills database with fake data'
-    faker = Factory.create()
-
-    CLIENT_COUNT = 0
-
-    def add_arguments(self, parser):
-        pass
-
-    def create_client(self):
-        for i in range(0, self.CLIENT_COUNT):
-            profile = self.faker.profile()
-            u = Client()
-            u.full_name = profile['name']
-            u.login = profile['username']
-            u.birthday = profile['birthdate']
-            u.contract_number = profile['ssn']
-            u.date_conclusion = self.faker.phone_number()
-            u.tel = self.faker.past_datetime()
-            u.addtess = profile['address']
-            u.save()
-
-            self.stdout.write('[%d] added user %s' % (u.id, u.username))
-
-    def handle(self, *args, **options):
-        self.create_client()
-
+FAKE_CLIENT()
